81ff5a84514091e89a93813c803298f73a23017c
Awesomeplayer165 <dev@awesomeplayer.tech>
2023-03-17 10:38:42 -0700

    Updated for everyone's tournaments

--- lib/models/constants.dart
+++ lib/models/constants.dart
@@ -8,6 +8,10 @@ class Constants {
     Tournament(name: "Monterey", key: "2023camb"),
     Tournament(name: "Fresno", key: "2023cafr"),
     Tournament(name: "2023 Week 0", key: "2023week0"),
+    Tournament(name: "2023 PNW District SunDome", key: "2023wayak"),
+    Tournament(name: "2023 PNW District Glacier Peak", key: "2023wasno"),
+    Tournament(name: "Finger Lakes Regional 2023", key: "2023nyro"),
+    Tournament(name: "New York Tech Valley Regional 2023", key: "2023nytr"),
     if (kDebugMode) Tournament(name: "Chezy 2022 (debug)", key: "2022cc"),
   ];
 }
